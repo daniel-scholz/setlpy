6f2f17a8b584b007611691a83fe05f7262409725
Simon Niedermayr <14186588+KeKsBoTer@users.noreply.github.com>
2019-01-27 23:23:31 +0100

    pip support

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="setlx in python",
+    version="0.0.1",
+    description="setlx functions/classes implemented in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daniel-scholz/py-sets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
