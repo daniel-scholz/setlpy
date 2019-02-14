0085260f3061462209162dfa26e2a97cf7449a9c
KeKsBoTer <14186588+KeKsBoTer@users.noreply.github.com>
2019-02-14 01:34:34 +0100

    decorators are now functions

--- setlx/decorators.py
+++ setlx/decorators.py
@@ -3,38 +3,41 @@ from inspect import getfullargspec
 from copy import deepcopy as _deepcopy
 
 
-class cached_procedure:
-    def __init__(self, fn):
-        self.fn = fn
-        self.decorator = _lru_cache(maxsize=None, typed=False)(fn)
-
-    def __call__(self, *args, **kwargs):
+def cached_procedure(func):
+    cache = _lru_cache(maxsize=None, typed=False)(func)
+    arg_info = getfullargspec(func)
+    
+    def decorator(*args, **kwargs):
         # no read/write arguments in cached procedures!
-        args = [_deepcopy(a) for a in args]
+        args = [_deepcopy(a) if arg_info.args[i] !=
+                "self" else a for i, a in enumerate(args)]
         kwargs = {name: _deepcopy(value) for name, value in kwargs.items()}
-        return self.decorator(*args, **kwargs)
+        return cache(*args, **kwargs)
+
+    return decorator
 
 
-class procedure:
-    def __init__(self, fn):
-        self.fn = fn
-        self.arg_info = getfullargspec(fn)
 
-    def __call__(self, *args, **kwargs):
-        arg_info = self.arg_info
+def procedure(func):
+    arg_info = getfullargspec(func)
+
+    def decorator(*args, **kwargs):
         ants = arg_info.annotations
 
         args_cp = []
         varargs = []
 
+        arg_names = arg_info.args
+
         if arg_info.varargs != None:
-            varargs = _deepcopy(args[len(arg_info.args):])
-            args = args[:len(arg_info.args)]
+            varargs = _deepcopy(args[len(arg_names):])
+            args = args[:len(arg_names)]
 
         for i, a in enumerate(args):
-            arg_name = arg_info.args[i]
+            arg_name = arg_names[i]
             # only copy value if argument hast read/write annotation
-            if arg_name in ants and ants[arg_name] == "rw":
+            # do not copy self argument
+            if (arg_name in ants and ants[arg_name] == "rw") or arg_name == "self":
                 args_cp.append(a)
             else:
                 args_cp.append(_deepcopy(a))
@@ -45,9 +48,53 @@ class procedure:
         kwargs_cp = {}
         for arg_name, value in kwargs.items():
             # only copy value if argument hast read/write annotation
-            if arg_name in ants and ants[arg_name] == "rw":
+            if arg_name in ants and ants[arg_name] == "rw" or arg_name == "self":
                 kwargs_cp[arg_name] = value
             else:
                 kwargs_cp[arg_name] = _deepcopy(value)
 
-        return self.fn(*args_cp, **kwargs_cp)
+        return func(*args, **kwargs)
+    return decorator
+
+"""
+class procedure:
+    def __init__(self, fn):
+        self.fn = fn
+        self.arg_info = getfullargspec(fn)
+
+    def __call__(self, *args, **kwargs):
+        return self.fn(*args, **kwargs)
+        arg_info = self.arg_info
+        ants = arg_info.annotations
+
+        args_cp = []
+        varargs = []
+
+        arg_names = [a for a in arg_info.args if a != "self"]
+
+        if arg_info.varargs != None:
+            varargs = _deepcopy(args[len(arg_names):])
+            args = args[:len(arg_names)]
+
+        for i, a in enumerate(args):
+            arg_name = arg_names[i]
+            # only copy value if argument hast read/write annotation
+            # do not copy self argument
+            if (arg_name in ants and ants[arg_name] == "rw") or arg_name == "self":
+                args_cp.append(a)
+            else:
+                args_cp.append(_deepcopy(a))
+
+        args_cp += varargs
+
+        # arguments which are assigned by name (e.g. x=2)
+        kwargs_cp = {}
+        for arg_name, value in kwargs.items():
+            # only copy value if argument hast read/write annotation
+            if arg_name in ants and ants[arg_name] == "rw" or arg_name == "self":
+                kwargs_cp[arg_name] = value
+            else:
+                kwargs_cp[arg_name] = _deepcopy(value)
+
+        return self.fn(*args, **kwargs)
+"""
