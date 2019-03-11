26e2ce70b4853fbd28336b16374431f71238c949
Daniel Scholz <24234559+daniel-scholz@users.noreply.github.com>
2019-03-11 17:22:04 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,28 +2,19 @@ from setlx.native import isList, isMap, isProbablePrime
 from setlx.node import BinaryNode
 from setlx.tree import Tree
 from setlx.set import Set
+import random
 
+numbers = [2, 12, 1]
 
-o = Tree(BinaryNode(10))
-o.insert(BinaryNode(2))
-o.insert(BinaryNode(12))
-o.insert(BinaryNode(1))
-
+o = Tree(10)
+print(o)
 s = Tree(BinaryNode(10))
-s.insert(BinaryNode(2))
-s.insert(BinaryNode(12))
-s.insert(BinaryNode(1))
-
-# print("other:", o)
-# print("self:", s)
+for n in numbers:
+    o.insert(n)
+    s.insert(n)
 # trees need to be iterable in order to work
-# print(s > o)
+print(s > o)
 
-for node in s:
-    print(node)
-my_set = Set(s)
-
-for node in my_set:
-     print(node)
-
-print(max(my_set))
+# random.Random(42).shuffle(my_list)
+my_set = Set(i for i in range(0, 10))
+print(my_set)
