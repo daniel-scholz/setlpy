fdc6998e555be1348a19c3f7224d7dd3b78ee993
Daniel Scholz <24234559+daniel-scholz@users.noreply.github.com>
2019-03-27 17:41:35 +0100

    implements equals operator

--- setlx/set.py
+++ setlx/set.py
@@ -135,6 +135,9 @@ class Set():
         # ^
         return self % other
 
+    def __eq__(self, other):
+        return self.tree == other.tree
+
     def __le__(self, other):  # a.k.a. is_subset
         """
         corresponds to self <= other
