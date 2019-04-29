a0fb3cf8e6411f89dd0b7759f49c06e8922197e7
Daniel Scholz <24234559+daniel-scholz@users.noreply.github.com>
2019-04-29 17:29:30 +0200

    Edits measuring tool, sort by calls

--- measure.py
+++ measure.py
@@ -3,5 +3,5 @@ import time
 
 file_name = f"result{int(time.time())}"
 with open(f"measures/{file_name}", "w+") as f:
-    subprocess.call(f"python  -m cProfile -s time __main__.py",
+    subprocess.call(f"python  -m cProfile -s calls __main__.py",
                     stdout=f)
